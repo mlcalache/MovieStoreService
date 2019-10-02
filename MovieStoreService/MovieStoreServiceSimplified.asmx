aaff17a8f40c998df9e9ed948ad933fda31606ff
Matheus de Lara Calache <matheus@retailisation.onmicrosoft.com>
2019-10-02 20:11:13 +0200

    Add MovieStoreServiceSimplified

---
+++ MovieStoreService/MovieStoreServiceSimplified.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="MovieStoreServiceSimplified.asmx.cs" Class="MovieStoreService.MovieStoreServiceSimplified" %>
---
+++ MovieStoreService/MovieStoreServiceSimplified.asmx.cs
@@ -0,0 +1,41 @@
+﻿using MovieStoreService.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Services;
+
+namespace MovieStoreService
+{
+    /// <summary>
+    /// Summary description for MovieStoreService
+    /// </summary>
+    [WebService(Namespace = "http://moviestoreservice.com/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line.
+    // [System.Web.Script.Services.ScriptService]
+    public class MovieStoreServiceSimplified : WebService
+    {
+        [WebMethod]
+        public List<Movie> GetAllMovies()
+        {
+            throw new NotImplementedException();
+        }
+
+        [WebMethod]
+        public List<Actor> GetCastByMovie(string movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        [WebMethod]
+        public Movie RegisterNewMovie(Movie movie)
+        {
+            if (movie == null)
+            {
+                throw new ArgumentNullException(nameof(movie));
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
